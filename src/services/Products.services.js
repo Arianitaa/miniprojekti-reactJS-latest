67efe196105888e6d78e0e5ff7d1afedd2e4a6c8
Arianitaa <arianitashabani@hotmail.com>
2021-12-01 20:15:03 +0100

    Product services addes.

---
+++ src/services/Products.services.js
@@ -0,0 +1,8 @@
+import http from "../http-common/http-common"
+
+class ProductDataServices {
+    getProducts(){
+        return http.get("/products");
+    }
+}
+export default new ProductDataServices;
